f28b8e80641f393a4ab5dc75f692e53cb2c5bcb3
Shivansh6317 <shivanxhskumar@gmail.com>
2025-10-31 00:16:44 +0530

    Initial project setup

---
+++ src/main/java/com/example/auth/E_Learning_platform.java
@@ -0,0 +1,13 @@
+package com.example.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E_Learning_platform {
+
+    public static void main(String[] args) {
+        SpringApplication.run(E_Learning_platform.class, args);
+    }
+
+}
